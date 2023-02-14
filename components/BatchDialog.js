88d314ba585734168e0368d7bbf608ea09c4b546
jreyesr <jreyesr@users.noreply.github.com>
2023-02-13 20:28:05 -0500

    Add progress bar to track requests sent

--- components/BatchDialog.js
+++ components/BatchDialog.js
@@ -6,28 +6,35 @@ import FormRow from './FormRow';
 import FileChooserButton from './FileChooserButton';
 import ActionButton from './ActionButton';
 import OutputFieldsChooser from './OutputFieldsChooser';
+import ProgressBar from './ProgressBar';
 
 export default function BatchDialog({context, request}) {
   const [csvHeaders, setCsvHeaders] = useState([]);
   const [csvData, setCsvData] = useState([]);
   const [outputConfig, setOutputConfig] = useState([]);
+  const [sent, setSent] = useState(0);
 
   const onFileChosen = (path => {
     csv()
       .on('header', setCsvHeaders)
       .fromFile(path)
-      .then(setCsvData);
+      .then(setCsvData)
+      .then(() => setSent(0));
   });
 
   const canRun = csvData.length > 0 && outputConfig.length > 0 && outputConfig.every(x => x.name && x.jsonPath);
   const onRun = async () => {
+    setSent(0);
     for(const row of csvData) {
       const storeKey = `${request._id}.batchExtraData`;
       await context.store.setItem(storeKey, JSON.stringify(row));
       await context.network.sendRequest(request);
       await context.store.removeItem(storeKey);
+      setSent(s => s + 1);
     }
   };
+
+  const totalRequests = csvData.length;
 
   const onChangeOutputFields = (x) => {
     setOutputConfig(x)
@@ -44,6 +51,10 @@ export default function BatchDialog({context, request}) {
     ) : <p>Choose a file above to preview it!</p>}
 
     <OutputFieldsChooser colNames={csvHeaders} onChange={onChangeOutputFields} />
+    <FormRow label="Progress">
+      <ProgressBar bgcolor="#a11" completed={sent * 100 / totalRequests} text={`${sent}/${totalRequests}`} />
+    </FormRow>
+    
     <ActionButton title="Run!" icon="fa-person-running" onClick={onRun} disabled={!canRun}/>
   </React.Fragment>);
 }
---
+++ components/ProgressBar.js
@@ -0,0 +1,42 @@
+// This component comes almost directly from 
+// https://dev.to/ramonak/react-how-to-create-a-custom-progress-bar-component-in-5-minutes-2lcl
+
+import React from "react";
+
+const ProgressBar = (props) => {
+  const { bgcolor, completed, text } = props;
+
+  const containerStyles = {
+    height: 15,
+    width: '100%',
+    backgroundColor: "#e0e0de",
+    borderRadius: 3,
+    marginTop: 10,
+    marginBottom: 10,
+  }
+
+  const fillerStyles = {
+    height: '100%',
+    width: `${completed}%`,
+    backgroundColor: bgcolor,
+    borderRadius: 'inherit',
+    textAlign: 'right',
+    transition: 'width .2s ease-in-out',
+  }
+
+  const labelStyles = {
+    paddingInline: 5,
+    color: 'white',
+    fontWeight: 'bold'
+  }
+
+  return (
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        <span style={labelStyles}>{text || `${completed}%`}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
